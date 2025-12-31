3e0a14e126b14e544f141cd24dc9161d8a0cbbca
elyramos <eramosy@soen.pe>
2025-12-30 23:16:12 -0500

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  env: {
-    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  }
+  output: "standalone",
 };
 
 export default nextConfig;
